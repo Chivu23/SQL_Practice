fc8ab04dfc79edf8484dabc1b5c7ca257ff5f1a3
Chivu23 <chivu1312g@gmail.com>
2024-02-05 16:32:31 +0200

    ExtrasUpDate

--- SQL_Extras.sql
+++ SQL_Extras.sql
@@ -15,12 +15,13 @@
 --      * SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country HAVING;
 --      * COUNT(CustomerId)>5;
 --
--- ALIASES are used to give a table, or a column in a table, a temporary name
+-- 4. ALIASES are used to give a table, or a column in a table, a temporary name
 --      * SELECT column1 as newName, column2, ... FROM table_name;
 --      * SELECT COUNT(CustomerID) as Nr Of Cust, Country FROM Customers GROUP BY Country;
--- LIMIT is used to restrict the number of result retrieved from database
+-- 5. LIMIT is used to restrict the number of result retrieved from database
 -- a. SELECT * FROM table_name LIMIT 5;
 
+*/
 
 select * from owners;
 select * from owners order by dateOfBirth;
@@ -31,3 +32,9 @@ select max(age) from owners;
 select o.ownerId.firsName, lastName, count(petId)
 from owners o inner join pets p on o.ownerId = p.ownerId
 group by o.ownerId, firsName, lastName;
+having count(petId)>2;
+
+--  normally filtering is done with the "where" clause, the only exception being the aggregate functions that can only be filtered with the "HAVING" clause
+
+select * from owners;
+select * from pets;
